187841a2e9802d7811cf6338085a5ef0b363f4c0
Mendel <mkonikov123@gmail.com>
2021-06-02 13:53:59 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Add your code here
+function submitData(name, email){
+
+    let postData = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+          },
+          body: JSON.stringify({
+            name: "Steve",
+            email: "steve@steve.com"
+          }) 
+        }
+        
+        return fetch('http://localhost:3000/users', postData)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(object) {
+            document.body.innerHTML = object.id;
+            console.log(object);
+        })
+        .catch(function(error) {
+        document.body.innerHTML = error.message;
+        console.log(error.message);
+    })
+}
+
+submitData();
